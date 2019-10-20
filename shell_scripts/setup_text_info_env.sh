8a559f800b38429cb18175cd9f86c1b64e09f0eb
Alex Antonison <adantonison@gmail.com>
2019-10-20 10:49:53 -0400

    Add comments to text info setup script

--- shell_scripts/setup_text_info_env.sh
+++ shell_scripts/setup_text_info_env.sh
@@ -1,9 +1,15 @@
+# This file is simply a collection of commands to be manually executed in a terminal
+
+# Remove text info
 conda remove --name text_info --all -y
 
+# Create a text_info environment in anaconda3
 conda create -n text_info python=3.7
 
+# Activate text_info
 conda activate text_info
 
+# Instll packages
 pip install pandas
 pip install beautifulsoup4
 pip install scipy
