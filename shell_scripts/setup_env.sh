683cf15f1c2608dc67291e0d31931c68cfafe8ed
Alex Antonison <adantonison@gmail.com>
2019-10-19 20:47:52 -0400

    Add chromedriver to setup script

--- shell_scripts/setup_env.sh
+++ shell_scripts/setup_env.sh
@@ -6,3 +6,17 @@
 ######
 conda remove --name text_info --all -y
 conda env create -f ../environment.yml
+
+
+## downloads chrome driver
+mkdir ../chromedriver_76/
+cd ../chromedriver_76/
+wget https://chromedriver.storage.googleapis.com/index.html?path=76.0.3809.126/
+
+mkdir ../chromedriver_77/
+cd ../chromedriver_77/
+wget https://chromedriver.storage.googleapis.com/index.html?path=77.0.3865.40/
+
+mkdir ../chromedriver_78/
+cd ../chromedriver_78/
+wget https://chromedriver.storage.googleapis.com/index.html?path=78.0.3904.11/
